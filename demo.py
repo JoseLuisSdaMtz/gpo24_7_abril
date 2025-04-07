860cf40a0084920daf4891992b0746e550256dc5
JoseLuisSaldañaMartinez <luis.saldanam@uanl.edu.mx>
2025-04-07 15:52:18 -0600

    prueba de codigo funcional 7 de abril

--- demo.py
+++ demo.py
@@ -2,11 +2,12 @@ class Cuadrado:
     def __init__(self, lado):
         self.lado = lado
         
+    def area(self):
+        return self.lado**2
+##################################################################
 
-    def Area(selr,area):
-        self.area =lado**2
+lado= int(input("ingresa el lado: "))
+cuadrado1 = Cuadrado(lado)
+r=cuadrado1.area()
 
-cuadrado1 = Cuadrado(16 )
-
-print(f"codigo tedioso... los lados: {cuadrado1.lado}")
-#print(f"codigo tedioso... el area: {cuadrado1.area}")
+print(f"codigo tedioso... el area es: {r}")
