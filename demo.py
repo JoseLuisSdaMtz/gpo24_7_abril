4abcf7068bfb6a6ab3f320b1158b5a18bd878982
JoseLuisSaldañaMartinez <luis.saldanam@uanl.edu.mx>
2025-04-07 15:41:16 -0600

    clase finalisada 7 de abril

--- demo.py
+++ demo.py
@@ -1,2 +1,12 @@
 class Cuadrado:
-    pass
+    def __init__(self, lado):
+        self.lado = lado
+        
+
+    def Area(selr,area):
+        self.area =lado**2
+
+cuadrado1 = Cuadrado(16 )
+
+print(f"codigo tedioso... los lados: {cuadrado1.lado}")
+#print(f"codigo tedioso... el area: {cuadrado1.area}")
